5352c6f60dfda1b5b6469a186e7ed8d818da10fe
KhoaDev <damngockhoa0703@gmail.com>
2024-12-31 22:09:14 +0700

    connect Accent8

--- fe-GameConnect/src/providers/provider.tsx
+++ fe-GameConnect/src/providers/provider.tsx
@@ -1,29 +1,29 @@
-import '@rainbow-me/rainbowkit/styles.css';
-import {
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import { config } from '../wagmi';
-import {
-  QueryClientProvider,
-  QueryClient
-} from "@tanstack/react-query";
+import "@rainbow-me/rainbowkit/styles.css";
+import { darkTheme, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiProvider } from "wagmi";
+import { config } from "../wagmi";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+
+const ancient8Theme = darkTheme({
+  accentColor: "white",
+  accentColorForeground: "#53c709",
+});
 
 interface ProviderProps {
-    children: React.ReactNode;
+  children: React.ReactNode;
 }
 const queryClient = new QueryClient();
 
 const Provider = ({ children }: ProviderProps) => {
-    return (
-        <WagmiProvider config={config}>
-          <QueryClientProvider client={queryClient}>
-            <RainbowKitProvider>
-              {children}
-            </RainbowKitProvider>
-          </QueryClientProvider>
-        </WagmiProvider>
-      );
-}
+  return (
+    <WagmiProvider config={config}>
+      <QueryClientProvider client={queryClient}>
+        <RainbowKitProvider theme={ancient8Theme}>
+          {children}
+        </RainbowKitProvider>
+      </QueryClientProvider>
+    </WagmiProvider>
+  );
+};
 
 export default Provider;
--- fe-GameConnect/src/wagmi.ts
+++ fe-GameConnect/src/wagmi.ts
@@ -1,9 +1,9 @@
-
-import { getDefaultConfig } from '@rainbow-me/rainbowkit';
-import { arbitrum, base, mainnet, optimism, polygon } from 'wagmi/chains';
+import { getDefaultConfig } from "@rainbow-me/rainbowkit";
+import { ancient8Sepolia } from "wagmi/chains";
 
 export const config = getDefaultConfig({
-  appName: 'RainbowKit demo',
-  projectId: 'YOUR_PROJECT_ID',
-  chains: [mainnet, polygon, optimism, arbitrum, base],
+  appName: "RainbowKit App",
+  projectId: "YOUR_PROJECT_ID",
+  chains: [ancient8Sepolia],
+  ssr: true,
 });
